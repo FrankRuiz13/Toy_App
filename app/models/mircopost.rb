3fc9d55746f2b8cd85a7da0b1fee94d19f792fa3
Frank Ruiz <frankruiz49@gmail.com>
2019-09-11 21:53:36 -0700

    added validations for name/email

--- app/models/mircopost.rb
+++ app/models/mircopost.rb
@@ -1,2 +1,4 @@
 class Mircopost < ApplicationRecord
+  belongs_to :user
+  validates :content, presence: true, length: { minimum: 10, maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :mircoposts
+  validates :name, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :email, presence: true, length: { maximum: 40 }
 end
